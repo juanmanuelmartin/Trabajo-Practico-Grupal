8668ca227d20126673e09c0fecc68b239fbeab52
juanmanuelmartin <juanmmartin02@gmail.com>
2019-08-26 18:38:37 -0300

    ventana en el main

--- Documents/NetBeansProjects/PRACTICO/src/practico/PRACTICO.java
+++ Documents/NetBeansProjects/PRACTICO/src/practico/PRACTICO.java
@@ -1,12 +1,58 @@
 package practico;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
 
 public class PRACTICO {
 
 
     public static void main(String[] args) {
+        PiedraPapelTijeras ppt = new PiedraPapelTijeras();
+        Ventana ventana = new Ventana();
         
+        ventana.botonpapel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                int elecPc = ppt.eleccionPc();
+                int elecUs = ppt.eleccionUsuario("papel");
+                int ganador = ppt.ganador(elecUs, elecPc);
+                if(ganador == 1){
+                    
+                }if(ganador == 0){
+                    
+                }
+            }
+            
+        } );
+        ventana.botonpiedra.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                int elecPc = ppt.eleccionPc();
+                int elecUs = ppt.eleccionUsuario("piedra");
+                int ganador = ppt.ganador(elecUs, elecPc);
+                if(ganador == 1){
+                    
+                }if(ganador == 0){
+                    
+                }
+            }
+            
+        } );
+        ventana.botontijeras.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                int elecPc = ppt.eleccionPc();
+                int elecUs = ppt.eleccionUsuario("tijeras");
+                int ganador = ppt.ganador(elecUs, elecPc);
+                if(ganador == 1){
+                    
+                }if(ganador == 0){
+                    
+                }
+            }
+            
+        } );
     }
     
 }
