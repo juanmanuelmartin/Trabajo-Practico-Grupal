e26877eef2961a87dc620cb56d604c81bfcd7313
juanmanuelmartin <juanmmartin02@gmail.com>
2019-08-27 08:26:24 -0300

    ventanaLogin

--- Documents/NetBeansProjects/PRACTICO/src/practico/Orco.java
+++ Documents/NetBeansProjects/PRACTICO/src/practico/Orco.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package practico;
 
-/**
- *
- * @author JUAN
- */
 public class Orco extends Personajes{
     
     private int salud;
